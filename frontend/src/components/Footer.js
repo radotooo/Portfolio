3a6fe65d947cb5fb9158d7e7d4da657970282a2f
radotooo <radotooo@gmail.com>
2020-10-10 01:22:56 +0300

    General improvements

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 import './styles/footer.css'
 
+
 export default function Footer() {
-    const envelopIcon = <FontAwesomeIcon icon={faEnvelope} />
+    const envelopIcon = <FontAwesomeIcon className="email" size="lg" icon={faEnvelope} />
+    const githubIcon = <FontAwesomeIcon icon={faGithub} size="lg" />
+    const linkedIcon = <FontAwesomeIcon icon={faLinkedin} size="lg" />
+
     return (
         <div className="footer">
             <div className="footer__container">
-                <p>
-                    {envelopIcon} radotooo@gmail.com
-                </p>
+                <p>Contact me:</p>
+                <a href="mailto:radotooo@gmail.com" target="_blank" rel="noopener noreferrer" className="footer__container__email">
+                    {envelopIcon}
+                </a>
+                <a href="https://github.com/radotooo" target="_blank" rel="noopener noreferrer">
+                    {githubIcon}
+                </a>
+                <a href="https://www.linkedin.com/in/radislav-danev-44a371a8/" target="_blank" rel="noopener noreferrer">
+                    {linkedIcon}
+                </a>
             </div>
         </div>
     )
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import "./styles/header.css"
 import NavMenu from './HeaderNavMenu'
+import { faFileDownload } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Header() {
+    const resumeIcon = <FontAwesomeIcon className="resume__icon" icon={faFileDownload} />
+
     return (
         <div className="nav">
             <div className="nav__content">
                 <p>Home</p>
                 <NavMenu />
-                <div className="text">
+                <div className="nav__content__text">
                     <p>Portfolio</p>
-                    <p>Resume</p>
+                    <p> {resumeIcon} Resume</p>
                 </div>
             </div>
 
