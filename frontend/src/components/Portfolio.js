9ea1706a892846a31c7ac0ec4c212c4a3ae1dac9
radotooo <radotooo@gmail.com>
2020-10-22 20:24:36 +0300

    Bug Fix

--- frontend/src/components/Portfolio.js
+++ frontend/src/components/Portfolio.js
@@ -9,7 +9,7 @@ export default function Portfolio() {
             <div className="portfolio">
                 <Card title="Quote Machine" img="https://live.staticflickr.com/65535/50474214391_059006306f_w.jpg" codeUrl="https://github.com/radotooo/ReactJs/tree/master/RandomQuoteMachine" liveView="https://pensive-boyd-fe627b.netlify.app/" />
 
-                <Card title="Amazon clone" img="https://live.staticflickr.com/65535/50473509543_7733a8a15d_w.jpg" codeUrl="https://github.com/radotooo/ReactJs/tree/master/AmazonClone/amazon-clone" liveView="https://clone-aa0a9.web.app/" />
+                <Card title="Amazon clone" img="https://live.staticflickr.com/65535/50516365768_083cd9d034_o.png" codeUrl="https://github.com/radotooo/ReactJs/tree/master/AmazonClone/amazon-clone" liveView="https://clone-aa0a9.web.app/" />
 
                 <Card title="Portfolio" img="https://live.staticflickr.com/65535/50495662926_93635dd588_w.jpg" codeUrl="https://github.com/radotooo/Portfolio/tree/main/frontend" liveView="" />
 
