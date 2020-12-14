2539627b1e9de9f0358fbac917b9e2bf114bf440
radotooo <radotooo@gmail.com>
2020-12-14 21:34:46 +0200

    refactor: change img directory

--- frontend/src/components/ResumeButton.js
+++ frontend/src/components/ResumeButton.js
@@ -1,15 +1,18 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFileDownload } from "@fortawesome/free-solid-svg-icons"
-import "./styles/resume-button.css"
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileDownload } from '@fortawesome/free-solid-svg-icons';
+import './styles/resume-button.css';
 
 export default function ResumeButton() {
+  const resumeIcon = (
+    <FontAwesomeIcon className="resume__icon" icon={faFileDownload} />
+  );
 
-    const resumeIcon = <FontAwesomeIcon className="resume__icon" icon={faFileDownload} />
-
-    return (
-        <>
-            <a href="Resume_R.Danev.docx" download>{resumeIcon} Resume</a>
-        </>
-    )
+  return (
+    <>
+      <a href="R.Danev_Resume.pdf" download>
+        {resumeIcon} Resume
+      </a>
+    </>
+  );
 }
